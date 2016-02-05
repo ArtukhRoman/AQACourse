e6a14af1da7d55b270604f72c494bfaa4aded5c8
ArtukhRoman <artukhroman@gmail.com>
2016-02-05 07:44:25 +0200

    main goal - reached. dirty work. Should clean code

--- src/Lecture3.java
+++ src/Lecture3.java
@@ -1,36 +1,65 @@
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 
 public class Lecture3 {
 
 	private static boolean enough;
-	private static int [] array;
+	private static int[] array;
 	private static Scanner input_stream;
 
 	public static void main(String[] args) {
-		
-		setArray(new int [10]);
-		setEnough(false);
-		setInput_stream(CreateConsoleInputStream());
-		while (!isEnough()) {
-			GetIntInput(getInput_stream());
-			
+
+		/*
+		 * setArray(new int [5]); setEnough(false);
+		 * setInput_stream(CreateConsoleInputStream()); //while (!isEnough()) {
+		 * int a = GetIntInput(getInput_stream());
+		 * 
+		 * if(a < 5){ System.out.println("is - " +
+		 * GetIntInput(getInput_stream()));
+		 * 
+		 * // }
+		 * 
+		 * } System.out.println(a);
+		 * 
+		 * System.out.println(Arrays.toString(getArray()));
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		int[] array = new int[10];
+		boolean flag = true;
+		Integer a = 0;
+		Integer b = 0;
+		while (flag) {
+
+			a = sc.nextInt();
+
+			if (a < 10) {
+
+				System.out.println("a<10");
+				if (b < array.length) {
+					array[b] = a;
+					b++;
+					System.out.println("b = " + b);
+				} else {
+					flag = false;
+				  }
+			}
+
 		}
-		
-		//System.out.println(array.length);
-		
+
+		System.out.println(Arrays.toString(array));
 
 	}
-	
+
 	private static Scanner CreateConsoleInputStream() {
 		Scanner consoleIn = new Scanner(System.in);
 		return consoleIn;
 
 	}
-	
+
 	private static Integer GetIntInput(Scanner stream) {
-		Integer value = null;		
+		Integer value = null;
 		try {
 			value = stream.nextInt();
 		} catch (InputMismatchException e) {
@@ -47,11 +76,11 @@ public class Lecture3 {
 		Lecture3.enough = enough;
 	}
 
-	public static int [] getArray() {
+	public static int[] getArray() {
 		return array;
 	}
 
-	public static void setArray(int [] array) {
+	public static void setArray(int[] array) {
 		Lecture3.array = array;
 	}
 
