39454aee78afbd4836cdb3ae66c7f98ffa2279e4
ArtukhRoman <artukhroman@gmail.com>
2016-02-05 09:42:35 +0200

    comlete task

--- src/Lecture3.java
+++ src/Lecture3.java
@@ -7,49 +7,44 @@ public class Lecture3 {
 	private static boolean enough;
 	private static int[] array;
 	private static Scanner input_stream;
+	private static Integer console_number;
+	private static int counter;
+
 
 	public static void main(String[] args) {
+		 
+		
+		  setArray(new int [10]);
+		  setEnough(false);
+		  setInput_stream(CreateConsoleInputStream()); 
+		  System.out.println("Please, enter some digit set");
+		  arrayFiller();
+          System.out.println("There is 10 numbers < 10 from the inputed sequence");
+		  System.out.println(Arrays.toString(getArray()));
 
-		/*
-		 * setArray(new int [5]); setEnough(false);
-		 * setInput_stream(CreateConsoleInputStream()); //while (!isEnough()) {
-		 * int a = GetIntInput(getInput_stream());
-		 * 
-		 * if(a < 5){ System.out.println("is - " +
-		 * GetIntInput(getInput_stream()));
-		 * 
-		 * // }
-		 * 
-		 * } System.out.println(a);
-		 * 
-		 * System.out.println(Arrays.toString(getArray()));
-		 */
+	}
 
-		Scanner sc = new Scanner(System.in);
-		int[] array = new int[10];
-		boolean flag = true;
-		Integer a = 0;
-		Integer b = 0;
-		while (flag) {
-
-			a = sc.nextInt();
-
-			if (a < 10) {
-
-				System.out.println("a<10");
-				if (b < array.length) {
-					array[b] = a;
-					b++;
-					System.out.println("b = " + b);
+	private static void arrayFiller() {
+		 console_number = null;
+		 counter = 0;
+		 while (!isEnough()) {
+	     console_number = GetIntInput(getInput_stream());
+	     
+		  if (console_number < 10) {
+			  
+				//System.out.println("a<10");
+				if (counter < array.length) {
+					array[counter] = console_number;
+					counter++;
+					//System.out.println("b = " + counter);
 				} else {
-					flag = false;
+					setEnough(true);
+					System.out.println("enogh " + isEnough());
 				  }
 			}
-
-		}
-
-		System.out.println(Arrays.toString(array));
-
+		  
+		  }
+		
 	}
 
 	private static Scanner CreateConsoleInputStream() {
